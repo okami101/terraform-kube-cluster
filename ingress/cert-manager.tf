87ae38307414925c87bd90c330dd7d33953477d1
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2022-09-21 20:00:30 +0200

    fix version

--- ingress/cert-manager.tf
+++ ingress/cert-manager.tf
@@ -6,7 +6,7 @@ resource "kubernetes_namespace_v1" "cert_manager" {
 
 resource "helm_release" "cert_manager" {
   chart   = "jetstack/cert-manager"
-  version = "1.9.1"
+  version = "v1.9.1"
 
   name      = "cert-manager"
   namespace = kubernetes_namespace_v1.cert_manager.metadata[0].name
