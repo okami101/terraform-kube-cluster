1c509d7966f99f29d360f212ebe4e2086ed5e43e
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2022-10-06 20:25:21 +0200

    up charts

--- ingress/traefik.tf
+++ ingress/traefik.tf
@@ -6,7 +6,7 @@ resource "kubernetes_namespace_v1" "traefik" {
 
 resource "helm_release" "traefik" {
   chart   = "traefik/traefik"
-  version = "10.30.1"
+  version = "12.0.0"
 
   name      = "traefik"
   namespace = kubernetes_namespace_v1.traefik.metadata[0].name
