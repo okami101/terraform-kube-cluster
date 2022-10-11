18be0b70ebd5e5625379698f53d1dbc6f713dfd2
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2022-10-11 21:24:07 +0200

    up version

--- ingress/traefik.tf
+++ ingress/traefik.tf
@@ -6,7 +6,7 @@ resource "kubernetes_namespace_v1" "traefik" {
 
 resource "helm_release" "traefik" {
   chart   = "traefik/traefik"
-  version = "12.0.2"
+  version = "14.0.1"
 
   name      = "traefik"
   namespace = kubernetes_namespace_v1.traefik.metadata[0].name
--- tools/velero.tf
+++ tools/velero.tf
@@ -6,7 +6,7 @@ resource "kubernetes_namespace_v1" "velero" {
 
 resource "helm_release" "velero" {
   chart   = "velero/velero"
-  version = "2.31.8"
+  version = "2.31.9"
 
   name      = "velero"
   namespace = kubernetes_namespace_v1.velero.metadata[0].name
