8a7669d17347a937f229c7edb62c3676439ea02c
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2023-03-19 15:49:16 +0100

    up deps

--- monitoring/grafana.tf
+++ monitoring/grafana.tf
@@ -1,6 +1,6 @@
 resource "helm_release" "grafana" {
   chart      = "grafana"
-  version    = "6.52.3"
+  version    = "6.52.4"
   repository = "https://grafana.github.io/helm-charts"
 
   name      = "grafana"
