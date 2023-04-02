1ca937f2c8cd3df59cb8f77cce536ca6f34df709
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2023-04-02 19:56:57 +0200

    up deps

--- monitoring/logging.tf
+++ monitoring/logging.tf
@@ -19,7 +19,7 @@ resource "helm_release" "loki" {
 
 resource "helm_release" "promtail" {
   chart      = "promtail"
-  version    = "6.9.3"
+  version    = "6.10.0"
   repository = "https://grafana.github.io/helm-charts"
 
   name      = "promtail"
