ba435ce1f934605e6229eb4ffa14488829b3e847
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2023-05-28 19:56:01 +0200

    up charts

--- base/cert-manager.tf
+++ base/cert-manager.tf
@@ -6,7 +6,7 @@ resource "kubernetes_namespace_v1" "cert_manager" {
 
 resource "helm_release" "cert_manager" {
   chart      = "cert-manager"
-  version    = "v1.12.0"
+  version    = "v1.12.1"
   repository = "https://charts.jetstack.io"
 
   name      = "cert-manager"
