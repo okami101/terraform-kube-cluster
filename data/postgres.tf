c85b4c18f7f5fb45865b88dd81894b74613832d3
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2023-07-02 13:41:24 +0200

    up charts

--- data/postgres.tf
+++ data/postgres.tf
@@ -403,7 +403,7 @@ resource "kubernetes_service_v1" "postgres_ro" {
 
 resource "helm_release" "postgres_exporter" {
   chart      = "prometheus-postgres-exporter"
-  version    = "4.5.0"
+  version    = "4.6.0"
   repository = "https://prometheus-community.github.io/helm-charts"
 
   name      = "postgres-exporter"
