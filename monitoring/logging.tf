1d0c0d5e15d0dad3d801c5b5354073f9253889f4
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2023-07-06 18:33:02 +0200

    up charts

--- monitoring/logging.tf
+++ monitoring/logging.tf
@@ -6,7 +6,7 @@ resource "kubernetes_namespace_v1" "logging" {
 
 resource "helm_release" "loki" {
   chart      = "loki"
-  version    = "5.8.8"
+  version    = "5.8.9"
   repository = "https://grafana.github.io/helm-charts"
 
   name      = "loki"
