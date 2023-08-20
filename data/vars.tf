1b49f28b232c0e30bf03a9250d3366b210e81f0d
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2023-08-20 22:14:13 +0200

    remove admin apps

--- data/vars.tf
+++ data/vars.tf
@@ -62,20 +62,6 @@ variable "mysql_password" {
   sensitive = true
 }
 
-variable "phpmyadmin_resources_requests" {
-  type = object({
-    cpu    = string
-    memory = string
-  })
-}
-
-variable "phpmyadmin_resources_limits" {
-  type = object({
-    cpu    = string
-    memory = string
-  })
-}
-
 variable "pgsql_admin_password" {
   type      = string
   sensitive = true
@@ -93,33 +79,6 @@ variable "pgsql_password" {
 variable "pgsql_replication_password" {
   type      = string
   sensitive = true
-}
-
-variable "pgadmin_default_email" {
-  type = string
-}
-
-variable "pgadmin_default_password" {
-  type      = string
-  sensitive = true
-}
-
-variable "pgadmin_pvc_name" {
-  type = string
-}
-
-variable "pgadmin_resources_requests" {
-  type = object({
-    cpu    = string
-    memory = string
-  })
-}
-
-variable "pgadmin_resources_limits" {
-  type = object({
-    cpu    = string
-    memory = string
-  })
 }
 
 variable "rabbitmq_user" {
