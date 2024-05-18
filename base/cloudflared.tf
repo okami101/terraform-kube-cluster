b7ba8d6699546cb0ef25ce14447a88eaaa2c7320
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2024-05-19 00:11:24 +0200

    add cloudflared

---
+++ base/cloudflared.tf
@@ -0,0 +1,43 @@
+resource "kubernetes_namespace_v1" "cloudflared" {
+  metadata {
+    name = "cloudflared"
+  }
+}
+
+resource "helm_release" "cloudflared" {
+  name       = "cloudflared"
+  repository = "https://charts.kubito.dev"
+  chart      = "kubitodev/cloudflared"
+  namespace  = kubernetes_namespace_v1.cloudflared.metadata[0].name
+  version    = "1.3.0"
+
+  set {
+    name  = "cloudflared.ingress[0].hostname"
+    value = "ssh.${var.domain}"
+  }
+
+  set {
+    name  = "cloudflared.ingress[0].service"
+    value = "ssh://traefik.traefik"
+  }
+
+  set {
+    name  = "cloudflared.ingress[1].hostname"
+    value = "*.${var.domain}"
+  }
+
+  set {
+    name  = "cloudflared.ingress[1].service"
+    value = "https://traefik.traefik"
+  }
+
+  set {
+    name  = "cloudflared.ingress[1].originRequest.originServerName"
+    value = var.domain
+  }
+
+  set {
+    name  = "metrics.enabled"
+    value = "true"
+  }
+}
