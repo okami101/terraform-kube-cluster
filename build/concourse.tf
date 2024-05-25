06cc023f88c0ac9febdfe5bf2a3e0da4281eb2a1
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2024-05-25 16:04:14 +0200

    add whitelist

--- build/concourse.tf
+++ build/concourse.tf
@@ -70,7 +70,7 @@ resource "kubernetes_secret_v1" "concourse_registry" {
   }
 
   data = {
-    name     = "gitea-http.gitea:3000"
+    name     = "gitea.${var.domain}"
     username = var.concourse_git_username
     password = var.concourse_git_password
   }
