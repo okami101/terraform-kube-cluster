a8ae0643331d76b84fc1fb2fc2c6b28cf8035757
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2024-05-25 15:47:00 +0200

    add whitelist

--- base/crowdsec.tf
+++ base/crowdsec.tf
@@ -25,6 +25,11 @@ resource "helm_release" "crowdsec" {
     name  = "lapi.env[0].value"
     value = var.bouncer_api_key
   }
+
+  set {
+    name  = "config.parsers.s02-enrich.whitelists.yaml"
+    value = var.crowdsec_whitelistes_config
+  }
 }
 
 resource "kubernetes_manifest" "traefik_middleware_bouncer" {
