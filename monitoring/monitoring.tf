7f69898cd1d38d8cd5a418c3302c10d57f3b4250
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2024-05-31 19:14:17 +0200

    fix ext url

--- monitoring/monitoring.tf
+++ monitoring/monitoring.tf
@@ -35,6 +35,16 @@ resource "helm_release" "kube_prometheus_stack" {
     name  = "kubeEtcd.endpoints"
     value = var.server_ips
   }
+
+  set {
+    name  = "prometheus.prometheusSpec.externalUrl"
+    value = "https://prom.int.${var.domain}"
+  }
+
+  set {
+    name  = "alertmanager.alertmanagerSpec.externalUrl"
+    value = "https://am.int.${var.domain}"
+  }
 }
 
 resource "kubernetes_manifest" "prometheus_ingress" {
