b5d6c4e1dbbccc4fc4fc7f2b8dbb14b1d45f4143
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2024-08-25 21:22:54 +0200

    wtf

--- data/cloudnative-pg.tf
+++ data/cloudnative-pg.tf
@@ -106,7 +106,7 @@ resource "kubernetes_manifest" "cnpg_cluster" {
     spec = {
       imageName   = "ghcr.io/cloudnative-pg/postgresql:16"
       description = "PostgreSQL Okami101"
-      instances   = 0
+      instances   = 2
 
       bootstrap = {
         initdb = {
