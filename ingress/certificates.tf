4e6e01fc31b3828d9088ac875e421b13fd608746
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2024-11-17 13:16:17 +0100

    internal domain ingress

--- ingress/certificates.tf
+++ ingress/certificates.tf
@@ -2,58 +2,58 @@ locals {
   certificate_secret_name = "tls-cert"
 }
 
-resource "kubernetes_manifest" "letsencrypt_production_issuer" {
-  manifest = {
-    apiVersion = "cert-manager.io/v1"
-    kind       = "ClusterIssuer"
-    metadata = {
-      name = "letsencrypt-production"
-    }
-    spec = {
-      acme = {
-        email = var.acme_email
-        privateKeySecretRef = {
-          name = "letsencrypt-production"
-        }
-        server = "https://acme-v02.api.letsencrypt.org/directory"
-        solvers = [
-          {
-            dns01 = {
-              webhook = {
-                groupName  = "acme.scaleway.com"
-                solverName = "scaleway"
-              }
-            }
-          }
-        ]
-      }
-    }
-  }
-}
+# resource "kubernetes_manifest" "letsencrypt_production_issuer" {
+#   manifest = {
+#     apiVersion = "cert-manager.io/v1"
+#     kind       = "ClusterIssuer"
+#     metadata = {
+#       name = "letsencrypt-production"
+#     }
+#     spec = {
+#       acme = {
+#         email = var.acme_email
+#         privateKeySecretRef = {
+#           name = "letsencrypt-production"
+#         }
+#         server = "https://acme-v02.api.letsencrypt.org/directory"
+#         solvers = [
+#           {
+#             dns01 = {
+#               webhook = {
+#                 groupName  = "acme.scaleway.com"
+#                 solverName = "scaleway"
+#               }
+#             }
+#           }
+#         ]
+#       }
+#     }
+#   }
+# }
 
-resource "kubernetes_manifest" "tls_certificate" {
-  manifest = {
-    apiVersion = "cert-manager.io/v1"
-    kind       = "Certificate"
-    metadata = {
-      name      = "default-certificate"
-      namespace = kubernetes_namespace_v1.traefik.metadata[0].name
-    }
-    spec = {
-      commonName = var.domain
-      dnsNames = [
-        var.domain,
-        "*.${var.domain}",
-        "*.${var.internal_domain}",
-      ]
-      issuerRef = {
-        kind = kubernetes_manifest.letsencrypt_production_issuer.manifest.kind
-        name = kubernetes_manifest.letsencrypt_production_issuer.manifest.metadata.name
-      }
-      secretName = local.certificate_secret_name
-      privateKey = {
-        rotationPolicy = "Always"
-      }
-    }
-  }
-}
+# resource "kubernetes_manifest" "tls_certificate" {
+#   manifest = {
+#     apiVersion = "cert-manager.io/v1"
+#     kind       = "Certificate"
+#     metadata = {
+#       name      = "default-certificate"
+#       namespace = kubernetes_namespace_v1.traefik.metadata[0].name
+#     }
+#     spec = {
+#       commonName = var.domain
+#       dnsNames = [
+#         var.domain,
+#         "*.${var.domain}",
+#         "*.${var.internal_domain}",
+#       ]
+#       issuerRef = {
+#         kind = kubernetes_manifest.letsencrypt_production_issuer.manifest.kind
+#         name = kubernetes_manifest.letsencrypt_production_issuer.manifest.metadata.name
+#       }
+#       secretName = local.certificate_secret_name
+#       privateKey = {
+#         rotationPolicy = "Always"
+#       }
+#     }
+#   }
+# }
