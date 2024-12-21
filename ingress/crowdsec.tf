28b458007c2cbe2eb4699fdf29b869075250e4be
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2024-12-21 20:32:17 +0100

    waf

--- ingress/crowdsec.tf
+++ ingress/crowdsec.tf
@@ -43,11 +43,15 @@ resource "kubernetes_manifest" "traefik_middleware_bouncer" {
     spec = {
       plugin = {
         bouncer = {
-          enabled          = true
-          crowdsecMode     = "stream"
-          updateMaxFailure = -1
-          crowdsecLapiHost = "crowdsec-service.crowdsec:8080"
-          crowdsecLapiKey  = var.bouncer_api_key
+          enabled                        = true
+          crowdsecAppsecEnabled          = true
+          crowdsecAppsecHost             = "crowdsec-appsec-service.crowdsec:7422"
+          crowdsecAppsecFailureBlock     = false
+          crowdsecAppsecUnreachableBlock = false
+          crowdsecMode                   = "stream"
+          updateMaxFailure               = -1
+          crowdsecLapiHost               = "crowdsec-service.crowdsec:8080"
+          crowdsecLapiKey                = var.bouncer_api_key
         }
       }
     }
